e07ed88d645547e36e3896812b95514ebe0d3c01
Andreas Buff <autobuff@gmx.de>
2016-03-13 13:58:54 +0100

    updates thresholds

--- badluefter.ino
+++ badluefter.ino
@@ -17,8 +17,8 @@ unsigned int movingAverage = 0;
 unsigned int lastMovingAverage = 0;  
 unsigned int stableCheckCounter = 0;  
 unsigned int minStability = 15;  
-int thresholdToTurnOn = 540;
-int thresholdToTurnOff = 555;
+int thresholdToTurnOn = 520;
+int thresholdToTurnOff = 535;
 boolean isLuefterOn = false;
 unsigned int readInterval = 2500;
   
